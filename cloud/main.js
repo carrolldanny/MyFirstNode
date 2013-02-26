8254bb9498b74eb89c2137555c5ff733329de40d
dandare@eircom.net <dandare@eircom.net>
2013-02-26 11:49:23 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,9 +7,9 @@ exports.myFunction = function(params, callback) {
 };
 
 exports.errorFunction = function(params, callback) {
-  var messsage = "theres been an error";
-  if(messsage == "theres been an error"){
-    return callback({err: messsage}, {result: num});
+  var num = params.num;
+  if(num > 10){
+    return callback({err: 'Num too large'}, {result: num});
   }
-  callback(undefined, {result: "all is well"});
+  callback(undefined, {result: num});
 };
